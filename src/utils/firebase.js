d2a00153da87a0aa2fda73cbe6eab147d0388bcc
M-Durmus <mdurmus9114@gmail.comm>
2022-04-09 23:12:15 +0300

    first commit

---
+++ src/utils/firebase.js
@@ -0,0 +1,13 @@
+import {initializeApp} from "firebase/app"
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAzXpSy29UHlV1H1vogS8YF9ZkeRCtx56E",
+  authDomain: "firecontact-483ab.firebaseapp.com",
+  projectId: "firecontact-483ab",
+  storageBucket: "firecontact-483ab.appspot.com",
+  messagingSenderId: "321721466989",
+  appId: "1:321721466989:web:9e2f047a040fee769e7f64"
+};
+
+  const firebase=initializeApp(firebaseConfig)
+  export default firebase;
